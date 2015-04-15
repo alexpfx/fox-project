a89bc2edd28c824bf0fe10e0b79b0881e6ffbb48
alexpfx <alexpfba@gmail.com>
2015-04-15 01:10:28 -0300

    Initial commit

---
+++ android/src/br/com/alexandrealessi/gdx/fox/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package br.com.alexandrealessi.gdx.fox.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import br.com.alexandrealessi.gdx.fox.MainGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MainGame(), config);
+	}
+}
