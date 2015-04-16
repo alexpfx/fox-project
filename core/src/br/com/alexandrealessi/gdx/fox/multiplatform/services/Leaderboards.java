2b2f01d53f71ed75c1dd54324ffa1a8768192498
alexpfx <alexpfba@gmail.com>
2015-04-16 01:06:18 -0300

    leaderboards

---
+++ core/src/br/com/alexandrealessi/gdx/fox/multiplatform/services/Leaderboards.java
@@ -0,0 +1,11 @@
+package br.com.alexandrealessi.gdx.fox.multiplatform.services;
+
+/**
+ * Interface para chamadas as APIs de Leaderboards...
+ */
+public interface Leaderboards {
+    void submitScore (String key, int score);
+    void incrementAchievment (String achievmentId, int amount);
+    void unlockAchievment (String achievmentId);
+
+}
