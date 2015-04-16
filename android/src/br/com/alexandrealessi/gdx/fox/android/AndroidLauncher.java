868d2e306c72ed9b576b6b1dfb79132f65966c3d
alexpfx <alexpfba@gmail.com>
2015-04-16 00:50:31 -0300

    Adicionada configuração para Google Play Services .

--- android/src/br/com/alexandrealessi/gdx/fox/android/AndroidLauncher.java
+++ android/src/br/com/alexandrealessi/gdx/fox/android/AndroidLauncher.java
@@ -5,14 +5,19 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import br.com.alexandrealessi.gdx.fox.MainGame;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.example.games.basegameutils.BaseGameActivity;
 
 public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new MainGame(), config);
-		BaseGameActivity b;
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        initialize(new MainGame(), config);
+
+		/*
+		Important: Because it is hard to anticipate the state of each device, you must always check for a
+		compatible Google Play services APK before you access Google Play services features.
+		 */
+    }
 }
