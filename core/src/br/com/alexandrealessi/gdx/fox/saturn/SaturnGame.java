0b287fc44433503cbbf1b20033ae51f7e0d3f210
alexpfx <alexpfba@gmail.com>
2015-04-23 21:07:05 -0300

    fox-project-5 Implementar modo de instanciar vários games no mesmo projeto criacao de classes base game e base screen e instanciacao de outro game.
    criado outra classe de game.

---
+++ core/src/br/com/alexandrealessi/gdx/fox/saturn/SaturnGame.java
@@ -0,0 +1,18 @@
+package br.com.alexandrealessi.gdx.fox.saturn;
+
+import br.com.alexandrealessi.gdx.fox.base.BaseGame;
+import br.com.alexandrealessi.gdx.fox.base.RequestHandler;
+
+/**
+ * Created by alexandre on 23/04/15.
+ */
+public class SaturnGame extends BaseGame {
+    protected SaturnGame(RequestHandler requestHand) {
+        super(requestHand);
+    }
+
+    @Override
+    public void create() {
+
+    }
+}
