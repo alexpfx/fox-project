66cadbb42fb8231bd9a9ac210889324fa9c3ad11
alexpfx <alexpfba@gmail.com>
2015-04-23 21:27:19 -0300

    script para texture packer

---
+++ desktop/src/br/com/alexandrealessi/gdx/fox/desktop/BuildAtlas.java
@@ -0,0 +1,7 @@
+package br.com.alexandrealessi.gdx.fox.desktop;
+
+/**
+ * Created by alexandre on 23/04/15.
+ */
+public class BuildAtlas {
+}
---
+++ run_texture_packer.bat
@@ -0,0 +1,1 @@
+java -cp gdx.jar;gdx-tools.jar com.badlogic.gdx.tools.texturepacker.TexturePacker assets-raw android/assets guitexturepack
