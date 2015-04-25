3faa5c9b82d285a50402748256dea44324e4ee2c
alexpfx <alexpfba@gmail.com>
2015-04-25 20:20:37 -0300

    fox-project-7 Integrar Overlap2d com modelo de física do R.U.B.E atualizacao em direcao a fazer a imagem andar conforme corpo
    classe renomeada.

--- core/src/br/com/alexandrealessi/gdx/fox/overlapexample/CarController.java
+++ core/src/br/com/alexandrealessi/gdx/fox/overlapexample/CarController.java
@@ -1,15 +1,18 @@
 package br.com.alexandrealessi.gdx.fox.overlapexample;
 
 import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.scenes.scene2d.utils.Align;
 import com.uwsoft.editor.renderer.actor.CompositeItem;
+import com.uwsoft.editor.renderer.physics.PhysicsBodyLoader;
 import com.uwsoft.editor.renderer.script.IScript;
 
 /**
  * Created by alexandre on 25/04/15.
  */
-public class PlayerController implements IScript {
+public class CarController implements IScript {
     private CompositeItem item;
     private Body body;
+    final float WORLD_TO_SCREEN = 31.5f;
 
     @Override
     public void init(CompositeItem item) {
@@ -24,7 +27,11 @@ public class PlayerController implements IScript {
 
     @Override
     public void act(float delta) {
-        item.setPosition(body.getPosition().x, body.getPosition().y);
 
+
+        final float x = body.getPosition().x * WORLD_TO_SCREEN + 400 - (item.getWidth() / 2 ) ;
+        final float y = body.getPosition().y * WORLD_TO_SCREEN + 240 - (item.getHeight() / 2) ;
+
+        item.setPosition(x, y);
     }
 }
--- core/src/br/com/alexandrealessi/gdx/fox/overlapexample/OverlapExampleStage.java
+++ core/src/br/com/alexandrealessi/gdx/fox/overlapexample/OverlapExampleStage.java
@@ -1,15 +1,13 @@
 package br.com.alexandrealessi.gdx.fox.overlapexample;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Camera;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.World;
-import com.badlogic.gdx.utils.Array;
 import com.gushikustudios.rube.RubeScene;
 import com.gushikustudios.rube.loader.RubeSceneLoader;
 import com.uwsoft.editor.renderer.Overlap2DStage;
 import com.uwsoft.editor.renderer.actor.CompositeItem;
+import com.uwsoft.editor.renderer.physics.PhysicsBodyLoader;
 import com.uwsoft.editor.renderer.resources.ResourceManager;
 
 /**
@@ -32,14 +30,17 @@ public class OverlapExampleStage extends Overlap2DStage {
 
         final CompositeItem carComposite = sceneLoader.getRoot().getCompositeById("car");
 
-
         RubeSceneLoader rubeSceneLoader = new RubeSceneLoader();
         RubeScene scene = rubeSceneLoader.loadScene(Gdx.files.internal("pug.json"));
+
         world = scene.getWorld();
 
 
         Body chassiBody = scene.getNamed(Body.class, "chassi").get(0);
         carComposite.setBody(chassiBody);
+        final CarController carController = new CarController();
+        carComposite.addScript(carController);
+        carController.init(carComposite);
 
 
         Body rodadianteira = scene.getNamed(Body.class, "rodaDianteira").get(0);
