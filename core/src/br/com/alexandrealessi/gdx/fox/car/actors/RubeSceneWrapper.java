27e45ea198e07579d5baae4045c627730d4548d2
alexpfx <alexpfba@gmail.com>
2015-04-26 13:15:07 -0300

    fox-project-11 - [30m]
    criado wrapper para os componentes do Rube.

---
+++ core/src/br/com/alexandrealessi/gdx/fox/car/actors/RubeSceneWrapper.java
@@ -0,0 +1,42 @@
+package br.com.alexandrealessi.gdx.fox.car.actors;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.utils.Array;
+import com.gushikustudios.rube.RubeScene;
+import com.gushikustudios.rube.loader.serializers.utils.RubeImage;
+
+/**
+ * Created by alexandre on 26/04/15.
+ * All Rube operations happens here to minimize dependency.
+ */
+public class RubeSceneWrapper {
+    private final RubeScene scene;
+
+    public RubeSceneWrapper(RubeScene scene) {
+        this.scene = scene;
+    }
+
+    public Body getBody(String name) {
+        return getBodies(name).get(0);
+    }
+
+    public Texture getTexture(String name) {
+        return getTextures(name).get(0);
+    }
+
+    public Array<Body> getBodies(String name) {
+        return scene.getNamed(Body.class, name);
+    }
+
+    public Array<Texture> getTextures(String name) {
+        final Array<RubeImage> rubeImages = scene.getNamed(RubeImage.class, name);
+        final Array<Texture> textures = new Array<>();
+        for (RubeImage image : rubeImages) {
+            Texture t = new Texture(Gdx.files.internal(image.name));
+            textures.add(t);
+        }
+        return textures;
+    }
+}
