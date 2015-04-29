a145929af4783606fdd2cde201825ca1330afa3e
alexpfx <alexpfba@gmail.com>
2015-04-29 11:50:19 -0300

    fox-project-16 Refactoring gerais
    colocando imagem no lugar correto.

--- core/src/br/com/alexandrealessi/gdx/fox/base/components/ImageDrawable.java
+++ core/src/br/com/alexandrealessi/gdx/fox/base/components/ImageDrawable.java
@@ -1,8 +1,11 @@
 package br.com.alexandrealessi.gdx.fox.base.components;
 
+import br.com.alexandrealessi.gdx.fox.games.CarsGameConstants;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 
@@ -18,13 +21,26 @@ public class ImageDrawable implements Drawable {
     }
 
     public static ImageDrawable createFromTextureRegion(TextureRegion textureRegion) {
+        final ImageDrawable imageDrawable = new ImageDrawable(new SpriteDrawable(new Sprite(textureRegion)));
+        return imageDrawable;
 
-        return new ImageDrawable(new SpriteDrawable(new Sprite(textureRegion)));
     }
 
     @Override
     public void draw(Batch batch, float alpha, Body body) {
-        spriteDrawable.draw(batch, 500f, 250f, getWidth(), getHeight());
+        final float x = body.getPosition().x;
+        final float y = body.getPosition().y;
+        final float angle = body.getAngle();
+        final Vector3 newPosition = calculatePosition(x, y, angle);
+        spriteDrawable.draw(batch, newPosition.x, newPosition.y, getWidth() / 2, getHeight() / 2, getWidth(), getHeight(), 1, 1, newPosition.z);
+    }
+
+    private Vector3 calculatePosition(float x, float y, float angle) {
+        final float xs = CarsGameConstants.Sizes.WORLD.scaleX(x, CarsGameConstants.Sizes.SCREEN) + CarsGameConstants.Sizes.SCREEN.width() / 2 - getWidth() / 2;
+        final float ys = CarsGameConstants.Sizes.WORLD.scaleY(y, CarsGameConstants.Sizes.SCREEN) + CarsGameConstants.Sizes.SCREEN.height() / 2 - getHeight() / 2;
+        final float rs = angle * MathUtils.radDeg;
+        Vector3.Zero.set(0, 0, 0);
+        return Vector3.Zero.add(xs, ys, rs);
     }
 
     @Override
