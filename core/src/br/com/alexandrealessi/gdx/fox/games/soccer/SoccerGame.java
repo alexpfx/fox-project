b48929c9349df0bfbd6bb4d91894ab3267081667
alexpfx <alexpfba@gmail.com>
2015-05-22 19:47:46 -0300

    soccer game.

---
+++ core/src/br/com/alexandrealessi/gdx/fox/games/soccer/SoccerGame.java
@@ -0,0 +1,13 @@
+package br.com.alexandrealessi.gdx.fox.games.soccer;
+
+import com.badlogic.gdx.Game;
+
+/**
+ * Created by alexandre on 22/05/15.
+ */
+public class SoccerGame extends Game {
+    @Override
+    public void create() {
+
+    }
+}
