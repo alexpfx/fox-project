47ce20e749d2163f0bad65a225bfe16a9451c23d
alexpfx <alexpfba@gmail.com>
2015-05-24 16:31:59 -0300

    adicionando textures do kenney pack.

--- run_game_texture_packer.bat
+++ run_game_texture_packer.bat
@@ -1,1 +1,1 @@
-java -cp gdx.jar;gdx-tools.jar com.badlogic.gdx.tools.texturepacker.TexturePacker assets-raw/topdown android/assets/topdownrace/images game.atlas
+java -cp gdx.jar;gdx-tools.jar com.badlogic.gdx.tools.texturepacker.TexturePacker assets-raw/animals android/assets/data/images game.atlas
