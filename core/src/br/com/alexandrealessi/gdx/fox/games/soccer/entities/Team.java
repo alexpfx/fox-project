e0f1b23488519456767921d2b3c2143346e3d4e6
alexpfx <alexpfba@gmail.com>
2015-05-25 21:37:33 -0300

    classes relacionadas a times.

---
+++ core/src/br/com/alexandrealessi/gdx/fox/games/soccer/entities/Team.java
@@ -0,0 +1,22 @@
+package br.com.alexandrealessi.gdx.fox.games.soccer.entities;
+
+import com.badlogic.gdx.utils.Array;
+
+/**
+ * Created by alexandre on 25/05/15.
+ */
+public class Team {
+
+    private String name;
+
+    private Array<Player> players = new Array<Player>();
+
+    public Team(String name, Array<Player> players) {
+        this.name = name;
+        this.players = players;
+    }
+
+    public Array<Player> getPlayers() {
+        return players;
+    }
+}
