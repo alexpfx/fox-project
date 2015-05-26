718c6a60f7b072ec79db0232039e7b2c89207d24
alexpfx <alexpfba@gmail.com>
2015-05-25 21:37:13 -0300

    classes relacionadas a players

---
+++ core/src/br/com/alexandrealessi/gdx/fox/games/soccer/entities/utils/PlayerBuilder.java
@@ -0,0 +1,31 @@
+package br.com.alexandrealessi.gdx.fox.games.soccer.entities.utils;
+
+import br.com.alexandrealessi.gdx.fox.base.utils.BodyBuilder;
+import br.com.alexandrealessi.gdx.fox.games.soccer.components.BodyComponent;
+import br.com.alexandrealessi.gdx.fox.games.soccer.components.PositionComponent;
+import br.com.alexandrealessi.gdx.fox.games.soccer.components.SpriteComponent;
+import br.com.alexandrealessi.gdx.fox.games.soccer.entities.Player;
+import br.com.alexandrealessi.gdx.fox.games.soccer.entities.Team;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.physics.box2d.Body;
+
+/**
+ * Created by alexandre on 25/05/15.
+ */
+public class PlayerBuilder {
+
+    public Player createPlayerEntity(PlayerData playerData, Sprite playerUniform, Body bodyModel) {
+        PlayerBuilder pb = new PlayerBuilder();
+        final Player player = new Player(playerData.name, playerData.number, playerData.position);
+        attachComponents(player, playerUniform, bodyModel);
+        return player;
+    }
+
+    private void attachComponents(Player playerEntity, Sprite uniform, Body bodyModel) {
+        playerEntity.add(new PositionComponent());
+        playerEntity.add(new SpriteComponent(uniform));
+        final BodyBuilder cloneBuilder = BodyBuilder.clone(bodyModel);
+        playerEntity.add(new BodyComponent(cloneBuilder.build()));
+    }
+
+}
---
+++ core/src/br/com/alexandrealessi/gdx/fox/games/soccer/entities/utils/PlayerData.java
@@ -0,0 +1,10 @@
+package br.com.alexandrealessi.gdx.fox.games.soccer.entities.utils;
+
+/**
+ * Created by alexandre on 25/05/15.
+ */
+public class PlayerData {
+    public String name;
+    public PlayerPosition position;
+    public int number;
+}
