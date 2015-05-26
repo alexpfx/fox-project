97c4f769554609d19323c46f42b44c932031edb0
alexpfx <alexpfba@gmail.com>
2015-05-25 21:38:10 -0300

    adicionando varios jogadores a cena.

--- core/src/br/com/alexandrealessi/gdx/fox/games/soccer/entities/Player.java
+++ core/src/br/com/alexandrealessi/gdx/fox/games/soccer/entities/Player.java
@@ -1,5 +1,7 @@
 package br.com.alexandrealessi.gdx.fox.games.soccer.entities;
 
+import br.com.alexandrealessi.gdx.fox.games.soccer.components.BodyComponent;
+import br.com.alexandrealessi.gdx.fox.games.soccer.entities.utils.PlayerPosition;
 import com.badlogic.ashley.core.Entity;
 
 /**
@@ -7,9 +9,30 @@ import com.badlogic.ashley.core.Entity;
  */
 public class Player extends Entity {
     private String name;
+    private int number;
+    private PlayerPosition position;
+    private Team team;
 
-    public Player(String name) {
+    public Player(String name, int number, PlayerPosition position) {
+        this.team = team;
         this.name = name;
+        this.number = number;
+        this.position = position;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public PlayerPosition getPosition() {
+        return position;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
 }
--- core/src/br/com/alexandrealessi/gdx/fox/games/soccer/screens/GamePlayScreen.java
+++ core/src/br/com/alexandrealessi/gdx/fox/games/soccer/screens/GamePlayScreen.java
@@ -1,11 +1,16 @@
 package br.com.alexandrealessi.gdx.fox.games.soccer.screens;
 
 import br.com.alexandrealessi.gdx.fox.base.entities.utils.RubeSceneWrapper;
-import br.com.alexandrealessi.gdx.fox.base.screens.Screen;
+import br.com.alexandrealessi.gdx.fox.base.screens.BaseScreen;
 import br.com.alexandrealessi.gdx.fox.games.soccer.SoccerGame;
 import br.com.alexandrealessi.gdx.fox.games.soccer.components.BodyComponent;
 import br.com.alexandrealessi.gdx.fox.games.soccer.components.PositionComponent;
 import br.com.alexandrealessi.gdx.fox.games.soccer.components.SpriteComponent;
+import br.com.alexandrealessi.gdx.fox.games.soccer.entities.Player;
+import br.com.alexandrealessi.gdx.fox.games.soccer.entities.Team;
+import br.com.alexandrealessi.gdx.fox.games.soccer.entities.utils.PlayerBuilder;
+import br.com.alexandrealessi.gdx.fox.games.soccer.entities.utils.PlayerData;
+import br.com.alexandrealessi.gdx.fox.games.soccer.entities.utils.PlayerPosition;
 import br.com.alexandrealessi.gdx.fox.games.soccer.systems.Box2dWorldSystem;
 import br.com.alexandrealessi.gdx.fox.games.soccer.systems.PhysicToScreenSystem;
 import br.com.alexandrealessi.gdx.fox.games.soccer.systems.RenderSystem;
@@ -16,20 +21,21 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 /**
  * Created by alexandre on 23/05/15.
  */
-public class GamePlayScreen extends Screen {
+public class GamePlayScreen extends BaseScreen {
     //1248 x 794
     private static final float SCENE_WIDTH = 160;
     private static final float SCENE_HEIGHT = 100;
     private static final float ASPECT_RATIO = SCENE_WIDTH / SCENE_HEIGHT;
 
     private Engine engine;
-    private Entity player;
     private Entity field;
     private TextureAtlas atlas;
     private RubeSceneWrapper rubeSceneWrapper;
@@ -43,14 +49,11 @@ public class GamePlayScreen extends Screen {
         atlas = new TextureAtlas(Gdx.files.internal("data/images/game.atlas"));
         rubeSceneWrapper = new RubeSceneWrapper("soccer.json");
         engine = new Engine();
-        player = new Entity();
 
-        player.add(new BodyComponent(rubeSceneWrapper.getBody("player")));
-        player.add(new PositionComponent());
+
 
         final Sprite panda = new Sprite(atlas.findRegion("panda"));
         panda.setScale(2.2f / panda.getHeight());
-        player.add(new SpriteComponent(panda));
 
         field = new Entity();
         field.add(new BodyComponent(rubeSceneWrapper.getBody("field")));
@@ -70,11 +73,46 @@ public class GamePlayScreen extends Screen {
         RenderSystem renderSystem = new RenderSystem(viewport);
 
         engine.addEntity(field);
-        engine.addEntity(player);
+        final Team galaticos = createTeam("galaticos", panda);
+
+        addTeamToEngine (engine, galaticos);
+
         engine.addSystem(physicToScreenSystem);
         engine.addSystem(renderSystem);
         engine.addSystem(box2dWorldSystem);
 
+    }
+
+    private void addTeamToEngine(Engine engine, Team galaticos) {
+        for (Player p:galaticos.getPlayers()){
+            engine.addEntity(p);
+        }
+    }
+
+    public Team createTeam(String name, Sprite uniform) {
+        PlayerBuilder builder = new PlayerBuilder();
+        final Body playerBody = rubeSceneWrapper.getBody("player");
+        Array<Player> players = new Array<Player>();
+        players.add(builder.createPlayerEntity(createPlayerData("Ochoa", 1, PlayerPosition.GK), uniform, playerBody));
+        players.add(builder.createPlayerEntity(createPlayerData("Messi", 10, PlayerPosition.ATTACKER), uniform, playerBody));
+        players.add(builder.createPlayerEntity(createPlayerData("Ronaldo", 9, PlayerPosition.ATTACKER), uniform, playerBody));
+        players.add(builder.createPlayerEntity(createPlayerData("Neymar", 11, PlayerPosition.ATTACKER), uniform, playerBody));
+        players.add(builder.createPlayerEntity(createPlayerData("Iniesta", 6, PlayerPosition.MIDDLEFIELD), uniform, playerBody));
+        players.add(builder.createPlayerEntity(createPlayerData("Xabi Alonso", 8, PlayerPosition.MIDDLEFIELD), uniform, playerBody));
+        players.add(builder.createPlayerEntity(createPlayerData("James Rodriguez", 7, PlayerPosition.MIDDLEFIELD), uniform, playerBody));
+        players.add(builder.createPlayerEntity(createPlayerData("Dani Alves", 2, PlayerPosition.DEFENDER), uniform, playerBody));
+        players.add(builder.createPlayerEntity(createPlayerData("Mascherano", 3, PlayerPosition.DEFENDER), uniform, playerBody));
+        players.add(builder.createPlayerEntity(createPlayerData("Boateng", 4, PlayerPosition.DEFENDER), uniform, playerBody));
+        players.add(builder.createPlayerEntity(createPlayerData("Lahm", 5, PlayerPosition.DEFENDER), uniform, playerBody));
+        return new Team(name, players);
+    }
+
+    public PlayerData createPlayerData(String name, int number, PlayerPosition position) {
+        PlayerData p = new PlayerData();
+        p.position = position;
+        p.number = number;
+        p.name = name;
+        return p;
     }
 
     @Override
