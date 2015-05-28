b2d064282e732577321527484c081ff579bbc1e2
alexpfx <alexpfba@gmail.com>
2015-05-28 18:58:41 -0300

    criando steercomponent que vai abrigar logica para AI.

---
+++ core/src/br/com/alexandrealessi/gdx/fox/base/ashley/components/SteerComponent.java
@@ -0,0 +1,122 @@
+package br.com.alexandrealessi.gdx.fox.base.ashley.components;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.ai.steer.Limiter;
+import com.badlogic.gdx.ai.steer.Steerable;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+
+/**
+ * Created by alexandre on 28/05/15.
+ */
+public class SteerComponent extends Component implements Steerable<Vector2>, Updatable{
+
+    private Body body;
+    private float boundingRadius;
+    private boolean tagged = false;
+    private float maxLinearSpeed;
+    private float maxLinearAcceleration;
+    private float maxAngularSpeed;
+    private float maxAngularAcceleration;
+
+    public SteerComponent (Body body){
+        this.body = body;
+    }
+
+    @Override
+    public Vector2 getPosition() {
+        return body.getPosition();
+    }
+
+    @Override
+    public float getOrientation() {
+        return body.getAngle();
+    }
+
+    @Override
+    public Vector2 getLinearVelocity() {
+        return body.getLinearVelocity();
+    }
+
+    @Override
+    public float getAngularVelocity() {
+        return body.getAngularVelocity();
+    }
+
+    @Override
+    public float getBoundingRadius() {
+        //confirmar
+        return body.getFixtureList().get(0).getShape().getRadius();
+    }
+
+    @Override
+    public boolean isTagged() {
+        return tagged;
+    }
+
+    @Override
+    public void setTagged(boolean tagged) {
+        this.tagged = tagged;
+    }
+
+    @Override
+    public Vector2 newVector() {
+        return new Vector2();
+    }
+
+    @Override
+    public float vectorToAngle(Vector2 vector) {
+        //confirmar
+        return vector.angle();
+    }
+
+    @Override
+    public Vector2 angleToVector(Vector2 outVector, float angle) {
+        return null;
+    }
+
+    @Override
+    public float getMaxLinearSpeed() {
+        return maxLinearSpeed;
+    }
+
+    @Override
+    public void setMaxLinearSpeed(float maxLinearSpeed) {
+        this.maxLinearSpeed = maxLinearSpeed;
+    }
+
+    @Override
+    public float getMaxLinearAcceleration() {
+        return maxLinearAcceleration;
+    }
+
+    @Override
+    public void setMaxLinearAcceleration(float maxLinearAcceleration) {
+        this.maxLinearAcceleration = maxLinearAcceleration;
+    }
+
+    @Override
+    public float getMaxAngularSpeed() {
+        return maxAngularSpeed;
+    }
+
+    @Override
+    public void setMaxAngularSpeed(float maxAngularSpeed) {
+        this.maxAngularSpeed = maxAngularSpeed;
+    }
+
+    @Override
+    public float getMaxAngularAcceleration() {
+        return maxAngularAcceleration;
+    }
+
+    @Override
+    public void setMaxAngularAcceleration(float maxAngularAcceleration) {
+        this.maxAngularAcceleration = maxAngularAcceleration;
+    }
+
+    @Override
+    public void update(float delta) {
+
+    }
+}
---
+++ core/src/br/com/alexandrealessi/gdx/fox/base/ashley/components/Updatable.java
@@ -0,0 +1,11 @@
+package br.com.alexandrealessi.gdx.fox.base.ashley.components;
+
+/**
+ * Created by alexandre on 28/05/15.
+ */
+public interface Updatable {
+
+    void update (float delta);
+
+
+}
