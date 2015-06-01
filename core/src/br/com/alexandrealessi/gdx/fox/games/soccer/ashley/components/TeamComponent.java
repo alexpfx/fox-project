55f938443ff82efd499590a0cd8022fd1fd5e1c0
alexpfx <alexpfba@gmail.com>
2015-05-31 21:28:57 -0300

    fox-project-30 Refactoring: organização da classe GamePlayScreen
    classes para match.

---
+++ core/src/br/com/alexandrealessi/gdx/fox/games/soccer/ashley/components/TeamComponent.java
@@ -0,0 +1,17 @@
+package br.com.alexandrealessi.gdx.fox.games.soccer.ashley.components;
+
+import br.com.alexandrealessi.gdx.fox.games.soccer.ashley.entities.PlayerEntity;
+import com.badlogic.gdx.utils.Array;
+
+/**
+ * Created by alexandre on 31/05/15.
+ */
+public class TeamComponent {
+
+
+
+    public TeamComponent() {
+
+    }
+
+}
---
+++ core/src/br/com/alexandrealessi/gdx/fox/games/soccer/ashley/entities/MatchStatus.java
@@ -0,0 +1,8 @@
+package br.com.alexandrealessi.gdx.fox.games.soccer.ashley.entities;
+
+/**
+ * Created by alexandre on 31/05/15.
+ */
+public enum MatchStatus {
+    STARTED, HALF_TIME, ENDED
+}
