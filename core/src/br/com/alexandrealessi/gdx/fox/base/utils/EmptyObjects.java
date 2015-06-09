716e6db7536fb8ea31ad3ba1745eb3b52218e851
alexpfx <alexpfba@gmail.com>
2015-06-08 23:30:56 -0300

    fox-project-34 Separar criação de Entities em Factories
    NullObjectPattern.

---
+++ core/src/br/com/alexandrealessi/gdx/fox/base/utils/EmptyObjects.java
@@ -0,0 +1,11 @@
+package br.com.alexandrealessi.gdx.fox.base.utils;
+
+import com.badlogic.gdx.utils.ArrayMap;
+
+/**
+ * Created by alexandre on 08/06/15.
+ */
+public class EmptyObjects {
+
+    public static final ArrayMap <String, Object> EMPTY_MAP = new ArrayMap<String, Object>();
+}
