aa72ca7e95e29e7daf73668a28675f762a5d7ac5
alexpfx <alexpfba@gmail.com>
2015-06-09 18:04:46 -0300

    fox-project-34 Separar criação de Entities em Factories
    classe para encapsular sprite com escala.

---
+++ core/src/br/com/alexandrealessi/gdx/fox/games/soccer/ashley/entities/factories/ScaledSprite.java
@@ -0,0 +1,23 @@
+package br.com.alexandrealessi.gdx.fox.games.soccer.ashley.entities.factories;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+/**
+ * Created by alexandre on 09/06/15.
+ */
+public class ScaledSprite {
+    private Sprite sprite;
+
+    private ScaledSprite(Sprite sprite, float scale) {
+        this.sprite = sprite;
+        sprite.setScale(scale);
+    }
+
+    public static ScaledSprite newInstance(Sprite sprite, float scale) {
+        return new ScaledSprite(sprite, scale);
+    }
+
+    public Sprite getSprite() {
+        return sprite;
+    }
+}
