d0155fe44421d9b9681b1503b6141b395a291232
alexpfx <alexpfba@gmail.com>
2015-06-08 23:46:12 -0300

    fox-project-34 Separar criação de Entities em Factories
    objeto para ser usado como parametro para os metodos create dos entitfy factory.

---
+++ core/src/br/com/alexandrealessi/gdx/fox/games/soccer/ashley/entities/factories/CreateArguments.java
@@ -0,0 +1,20 @@
+package br.com.alexandrealessi.gdx.fox.games.soccer.ashley.entities.factories;
+
+import com.badlogic.gdx.utils.ArrayMap;
+
+/**
+ * Created by alexandre on 08/06/15.
+ */
+public class CreateArguments {
+
+    private ArrayMap<String, Object> arguments = new ArrayMap<String, Object>();
+
+    public <T> void put(String key, T value) {
+        arguments.put(key, value);
+    }
+
+    public <T> T get (String key){
+        return (T) arguments.get(key);
+    }
+
+}
