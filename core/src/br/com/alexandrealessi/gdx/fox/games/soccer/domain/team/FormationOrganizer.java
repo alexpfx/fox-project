f7025323a98d88abb9ec639d6a60177ba570735b
alexpfx <alexpfba@gmail.com>
2015-06-11 21:59:22 -0300

    fox-project-35 Refatorar classes BodyBuilder e FixtureBuilder
    formations

--- core/src/br/com/alexandrealessi/gdx/fox/games/soccer/domain/team/FormationOrganizer.java
+++ core/src/br/com/alexandrealessi/gdx/fox/games/soccer/domain/team/FormationOrganizer.java
@@ -2,12 +2,6 @@ package br.com.alexandrealessi.gdx.fox.games.soccer.domain.team;
 
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
-import com.sun.deploy.util.ArrayUtil;
-import net.dermetfan.gdx.utils.ArrayUtils;
-
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 
 /**
  * Created by alexandre on 10/06/15.
@@ -15,13 +9,9 @@ import java.util.Collections;
 public class FormationOrganizer {
 
     private TeamFormation teamFormation;
-    private Array<Vector2> positionArray;
-    private float scale = 0;
 
-    public FormationOrganizer(TeamFormation teamFormation, Array<Vector2> positionArray, float scale) {
+    public FormationOrganizer(TeamFormation teamFormation) {
         this.teamFormation = teamFormation;
-        this.positionArray = positionArray;
-        this.scale = scale;
     }
 
     public Array<Vector2> organize(FormationOrganizerType type) {
@@ -31,22 +21,22 @@ public class FormationOrganizer {
         Array<Vector2> array = new Array<Vector2>();
 
         for (PlayerPosition pp : defenderFormation.getPositionArray()) {
-            Vector2 position = getPosition (pp, type);
+            Vector2 position = getPosition(pp, type);
             array.add(position);
         }
         for (PlayerPosition pp : middlefieldFormation.getPositionArray()) {
-            Vector2 position = getPosition (pp, type);
+            Vector2 position = getPosition(pp, type);
             array.add(position);
         }
         for (PlayerPosition pp : attackerFormation.getPositionArray()) {
-            Vector2 position = getPosition (pp, type);
+            Vector2 position = getPosition(pp, type);
             array.add(position);
         }
         return array;
     }
 
     private Vector2 getPosition(PlayerPosition pp, FormationOrganizerType type) {
-        return null;
+        return pp.getInitialPosition();
     }
 
     enum FormationOrganizerType {
