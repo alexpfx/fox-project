2d70a651f8b9ece87465d935c9e606eb4e838c2b
alexpfx <alexpfba@gmail.com>
2015-06-13 23:13:23 -0300

    fox-project-35 Refatorar classes BodyBuilder e FixtureBuilder
    classe team factory

--- core/src/br/com/alexandrealessi/gdx/fox/games/soccer/ashley/entities/factories/TeamFactory.java
+++ core/src/br/com/alexandrealessi/gdx/fox/games/soccer/ashley/entities/factories/TeamFactory.java
@@ -1,20 +1,58 @@
 package br.com.alexandrealessi.gdx.fox.games.soccer.ashley.entities.factories;
 
+import br.com.alexandrealessi.gdx.fox.base.box2d.RubeSceneHelper;
+import br.com.alexandrealessi.gdx.fox.games.soccer.ashley.components.PlayersComponent;
+import br.com.alexandrealessi.gdx.fox.games.soccer.ashley.components.TeamFormationComponent;
+import br.com.alexandrealessi.gdx.fox.games.soccer.ashley.components.TeamInfoComponent;
+import br.com.alexandrealessi.gdx.fox.games.soccer.ashley.components.TeamMatchContext;
+import br.com.alexandrealessi.gdx.fox.games.soccer.ashley.components.domain.TeamSide;
+import br.com.alexandrealessi.gdx.fox.games.soccer.domain.team.TeamFormation;
 import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.utils.Array;
 
 /**
  * Created by alexandre on 11/06/15.
  */
 public class TeamFactory extends CreateAndAddToEngineEntityFactory{
 
+    public static final String TEAM_SIDE = "team.side";
+    public static final String TEAM_NAME = "team.name";
+    public static final String TEAM_MAIN_UNIFORM = "team.main.uniform";
+    public static final String TEAM_GK_UNIFORM = "team.gk.uniform";
+    public static final String TEAM_FORMATION = "team.formation";
+
+    private int numberOfPlayers = 11;
+    private RubeSceneHelper rubeSceneHelper;
+
     @Override
     public Entity create(CreateArguments arguments) {
         Entity entity = new Entity();
 
+        createAndAddTeamInfo(entity, arguments);
+        createAndAddTeamMatchContext (entity, arguments);
+        createAndAddTeamFormation (entity, arguments);
 
-
-
-
-        return null;
+        return entity;
     }
+
+    private void createAndAddTeamFormation(Entity entity, CreateArguments arguments) {
+        TeamFormation formation = arguments.get(TEAM_FORMATION);
+        TeamFormationComponent teamFormationComponent = TeamFormationComponent.newInstance(formation);
+        entity.add(teamFormationComponent);
+    }
+
+    private void createAndAddTeamMatchContext(Entity entity, CreateArguments arguments) {
+        TeamSide teamSide = arguments.get(TEAM_SIDE);
+        TeamMatchContext teamMatchContext = TeamMatchContext.newInstance(teamSide);
+        entity.add(teamMatchContext);
+    }
+
+    private void createAndAddTeamInfo(Entity entity, CreateArguments arguments){
+        String name = arguments.get(TEAM_NAME);
+        ScaledSprite mainUniform = arguments.get(TEAM_MAIN_UNIFORM);
+        ScaledSprite gkUniform = arguments.get(TEAM_GK_UNIFORM);
+        TeamInfoComponent teamInfoComponent = TeamInfoComponent.newInstance(name, mainUniform, gkUniform);
+        entity.add(teamInfoComponent);
+    }
+
 }
