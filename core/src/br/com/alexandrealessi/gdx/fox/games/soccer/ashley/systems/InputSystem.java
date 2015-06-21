8a9819e6737975849db568889295bb3ed3d93f17
alexpfx <alexpfba@gmail.com>
2015-06-21 14:43:50 -0300

    fox-project-36 Adicionar controles do gamepad
    formatação.

--- core/src/br/com/alexandrealessi/gdx/fox/games/soccer/ashley/systems/InputSystem.java
+++ core/src/br/com/alexandrealessi/gdx/fox/games/soccer/ashley/systems/InputSystem.java
@@ -9,7 +9,6 @@ import com.badlogic.ashley.core.EntitySystem;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.utils.ImmutableArray;
 import com.badlogic.gdx.controllers.Controller;
-import com.badlogic.gdx.math.MathUtils;
 
 /**
  * Created by alexandre on 19/06/15.
@@ -34,9 +33,9 @@ public class InputSystem extends EntitySystem {
         final ControllersComponent controllersComponent = ComponentMappers.CONTROLLERS.get(controller);
         final Controller controller = controllersComponent.getController();
         float x = controller.getAxis(XboxOneMapping.AXIS_LEFT_X);
-        float y = controller.getAxis(XboxOneMapping.AXIS_LEFT_Y) *  -1;
-        x = Math.abs(x) > 0.05f?x:0;
-        y = Math.abs(y) > 0.05f?y:0;
+        float y = controller.getAxis(XboxOneMapping.AXIS_LEFT_Y) * -1;
+        x = Math.abs(x) > 0.05f ? x : 0;
+        y = Math.abs(y) > 0.05f ? y : 0;
         boolean a = controller.getButton(XboxOneMapping.BUTTON_A);
         boolean b = controller.getButton(XboxOneMapping.BUTTON_B);
         boolean bx = controller.getButton(XboxOneMapping.BUTTON_X);
@@ -48,17 +47,17 @@ public class InputSystem extends EntitySystem {
         final Entity player = players.get(14);
         final BodyComponent bodyComponent = ComponentMappers.BODY.get(player);
 
-        if (a){
+        if (a) {
             final float delta = 0.5f;
             bodyComponent.getBody().setLinearVelocity(x * MULT * delta, y * MULT * delta);
             System.out.println("xxx");
-        }  else if (b){
+        } else if (b) {
             final float delta = 1f;
             bodyComponent.getBody().setLinearVelocity(x * MULT * delta, y * MULT * delta);
-        } else if (bx){
+        } else if (bx) {
             final float delta = 2f;
             bodyComponent.getBody().setLinearVelocity(x * MULT * delta, y * MULT * delta);
-        } else if (by){
+        } else if (by) {
             final float delta = 4f;
             bodyComponent.getBody().setLinearVelocity(x * MULT * delta, y * MULT * delta);
         }
