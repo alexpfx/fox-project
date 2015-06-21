92fe5f1f56b56f4c50f6473b22ebd8e75d05c83b
alexpfx <alexpfba@gmail.com>
2015-06-21 20:56:11 -0300

    fox-project-37 Resetar os times quando ocorrer um gol.
     metodos setPosition e setLinearVelocity

--- core/src/br/com/alexandrealessi/gdx/fox/games/soccer/ashley/components/BodyComponent.java
+++ core/src/br/com/alexandrealessi/gdx/fox/games/soccer/ashley/components/BodyComponent.java
@@ -2,6 +2,7 @@ package br.com.alexandrealessi.gdx.fox.games.soccer.ashley.components;
 
 import br.com.alexandrealessi.gdx.fox.base.UserData;
 import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.gushikustudios.rube.RubeDefaults;
 
@@ -32,4 +33,13 @@ public class BodyComponent extends Component implements UserData {
     public void setIsAlive(boolean isAlive) {
         this.isAlive = isAlive;
     }
+
+    public void setPosition (Vector2 position){
+        body.setTransform(position, 0);
+    }
+
+    public void setLinearVelocity (float x, float y){
+        body.setLinearVelocity(x, y);
+    }
+
 }
