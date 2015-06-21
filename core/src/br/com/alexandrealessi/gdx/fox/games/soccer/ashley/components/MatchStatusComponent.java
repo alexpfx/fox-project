68d30d74204570b0efca05427822bf794add0b29
alexpfx <alexpfba@gmail.com>
2015-06-21 15:56:53 -0300

    fox-project-37 Resetar os times quando ocorrer um gol.
     adicionado metodo para compararar.

--- core/src/br/com/alexandrealessi/gdx/fox/games/soccer/ashley/components/MatchStatusComponent.java
+++ core/src/br/com/alexandrealessi/gdx/fox/games/soccer/ashley/components/MatchStatusComponent.java
@@ -5,7 +5,7 @@ import com.badlogic.ashley.core.Component;
 /**
  * Created by alexandre on 21/06/15.
  */
-public class MatchStatusComponent extends Component{
+public class MatchStatusComponent extends Component {
 
     private MatchGameStatus gameStatus = MatchGameStatus.AFTER_BEGIN;
 
@@ -18,7 +18,15 @@ public class MatchStatusComponent extends Component{
     }
 
     public enum MatchGameStatus {
-        AFTER_BEGIN, AFTER_GOAL, AFTER_HALF, AFTER_END
-    }
+        AFTER_BEGIN, AFTER_GOAL, AFTER_HALF, AFTER_END;
 
+        public boolean isOneOf(MatchGameStatus... matchGameStatuses) {
+            for (MatchGameStatus status : matchGameStatuses) {
+                if (status == this) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
 }
