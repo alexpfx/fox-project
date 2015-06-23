02eb17d6afffbf07f80b0dc7ce4902a844b63ff7
alexpfx <alexpfba@gmail.com>
2015-06-23 18:57:26 -0300

    fox-project-37 Resetar os times quando ocorrer um gol. formatação.

--- core/src/br/com/alexandrealessi/gdx/fox/games/soccer/ashley/entities/BallEntity.java
+++ core/src/br/com/alexandrealessi/gdx/fox/games/soccer/ashley/entities/BallEntity.java
@@ -8,6 +8,7 @@ import br.com.alexandrealessi.gdx.fox.games.soccer.ashley.entities.factories.Sca
 import com.badlogic.ashley.core.Component;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.Fixture;
@@ -16,36 +17,37 @@ import com.badlogic.gdx.utils.Array;
 /**
  * Created by alexandre on 22/06/15.
  */
-    public class BallEntity extends UserEntity {
-        private final RubeSceneHelper rubeSceneHelper;
-        private ScaledSprite ballSprite;
-        private Body ballBody;
-        private Camera camera;
+public class BallEntity extends UserEntity {
+    public static final float SCALE = 0.78f;
+    private final RubeSceneHelper rubeSceneHelper;
+    private ScaledSprite ballSprite;
+    private Body ballBody;
+    private Camera camera;
 
-        public BallEntity(ScaledSprite ballSprite, Body ballBody, Camera camera, RubeSceneHelper rubeSceneHelper) {
-            this.ballSprite = ballSprite;
-            this.ballBody = ballBody;
-            this.camera = camera;
-            this.rubeSceneHelper = rubeSceneHelper;
-        }
-
-        @Override
-        public Array<Component> getComponents() {
-            Array<Component> components = new Array<Component>();
-            components.add(PositionComponent.newInstance());
-            components.add(CameraFollowerComponent.newInstance(camera));
-            components.add(SpriteComponent.newInstance(ballSprite.getSprite()));
-            components.add(BodyComponent.newInstance(ballBody));
-            components.add(BallContextComponent.newInstance());
-            return components;
-        }
-
-        @Override
-        public void init(Entity entity) {
-            BodyComponent bodyComponent = entity.getComponent(BodyComponent.class);
-            bodyComponent.setPosition(Vector2.Zero);
-
-            Fixture ballFixture = rubeSceneHelper.getFixture(bodyComponent.getBody(), "ball");
-            ballFixture.setUserData(new FixtureUserData(FixtureType.BALL, entity));
-        }
+    public BallEntity(Sprite sprite, Body ballBody, Camera camera, RubeSceneHelper rubeSceneHelper) {
+        this.ballSprite = ScaledSprite.create(sprite, SCALE / sprite.getHeight());
+        this.ballBody = ballBody;
+        this.camera = camera;
+        this.rubeSceneHelper = rubeSceneHelper;
     }
+
+    @Override
+    public Array<Component> getComponents() {
+        Array<Component> components = new Array<Component>();
+        components.add(PositionComponent.newInstance());
+        components.add(CameraFollowerComponent.newInstance(camera));
+        components.add(SpriteComponent.newInstance(ballSprite.getSprite()));
+        components.add(BodyComponent.newInstance(ballBody));
+        components.add(BallContextComponent.newInstance());
+        return components;
+    }
+
+    @Override
+    public void init(Entity entity) {
+        BodyComponent bodyComponent = entity.getComponent(BodyComponent.class);
+        bodyComponent.setPosition(Vector2.Zero);
+
+        Fixture ballFixture = rubeSceneHelper.getFixture(bodyComponent.getBody(), "ball");
+        ballFixture.setUserData(new FixtureUserData(FixtureType.BALL, entity));
+    }
+}
--- core/src/br/com/alexandrealessi/gdx/fox/games/soccer/ashley/entities/UserEntity.java
+++ core/src/br/com/alexandrealessi/gdx/fox/games/soccer/ashley/entities/UserEntity.java
@@ -7,40 +7,40 @@ import com.badlogic.gdx.utils.Array;
 /**
  * Created by alexandre on 22/06/15.
  */
-    public abstract class UserEntity {
+public abstract class UserEntity {
 
-        private final Entity entity;
-        private boolean wasBuilt = false;
-        private Array<Class<? extends Component>> componentClasses;
+    private final Entity entity;
+    private boolean wasBuilt = false;
+    private Array<Class<? extends Component>> componentClasses;
 
-        protected UserEntity() {
-            this.entity = new Entity();
-        }
-
-        public final Entity getEntity() {
-            if (!wasBuilt) {
-                final Array<Component> components = getComponents();
-                componentClasses = new Array<Class<? extends Component>>();
-                for (Component c : components) {
-                    entity.add(c);
-                    componentClasses.add(c.getClass());
-                }
-                init(entity);
-            }
-            return entity;
-        }
-
-        protected abstract Array<Component> getComponents();
-
-        public Class<? extends Component>[] getComponentClasses() {
-            if (!wasBuilt) {
-                throw new IllegalArgumentException("It needs to build entity before use it.");
-            }
-            return componentClasses.toArray();
-        }
-
-        /* Override when need to init some components */
-        public void init(Entity entity) {
-
-        }
+    protected UserEntity() {
+        this.entity = new Entity();
     }
+
+    public final Entity getEntity() {
+        if (!wasBuilt) {
+            final Array<Component> components = getComponents();
+            componentClasses = new Array<Class<? extends Component>>();
+            for (Component c : components) {
+                entity.add(c);
+                componentClasses.add(c.getClass());
+            }
+            init(entity);
+        }
+        return entity;
+    }
+
+    protected abstract Array<Component> getComponents();
+
+    public Class<? extends Component>[] getComponentClasses() {
+        if (!wasBuilt) {
+            throw new IllegalArgumentException("It needs to build entity before use it.");
+        }
+        return componentClasses.toArray();
+    }
+
+    /* Override when need to init some components */
+    public void init(Entity entity) {
+
+    }
+}
