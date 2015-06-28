8bb4d5f25745c955f2d37c0094f09336e556b0a6
alexpfx <alexpfba@gmail.com>
2015-06-27 23:10:27 -0300

    fox-project-38 Refatorar para organizar componentes
    criando NullComponent nos casos que as Family de entidades não serão necessárias.

---
+++ core/src/br/com/alexandrealessi/gdx/fox/games/soccer/ashley/components/NullComponent.java
@@ -0,0 +1,9 @@
+package br.com.alexandrealessi.gdx.fox.games.soccer.ashley.components;
+
+import com.badlogic.ashley.core.Component;
+
+/**
+ * Created by alexandre on 27/06/15.
+ */
+public class NullComponent extends Component {
+}
